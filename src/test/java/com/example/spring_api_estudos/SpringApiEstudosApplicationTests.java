a3aa2550574d0e1fb73c32de017b4d8b65613c56
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-01-19 07:59:07 -0300

    first commit

---
+++ src/test/java/com/example/spring_api_estudos/SpringApiEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_api_estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
